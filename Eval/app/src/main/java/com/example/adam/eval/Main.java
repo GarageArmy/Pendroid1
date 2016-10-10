16409e312eaee977ab308338aefaa01004436356
Aduci01 <zuzzu01@gmail.com>
2016-10-10 17:23:12 +0200

    1

---
+++ Eval/app/src/main/java/com/example/adam/eval/Main.java
@@ -0,0 +1,148 @@
+package com.example.adam.eval;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Random;
+
+public class Main extends AppCompatActivity {
+
+    // formula variables:
+    private int[] values;
+    private int amount;
+    private String formula = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    public void formulaButtonClicked(View v){
+        generateFormula();
+        for (int i = 0; i < 4; i++){
+            values[i + 1] = (int)eval(Integer.toString(values[i]) + formula);
+            System.out.println(values[i]);
+        }
+    }
+
+    private void generateFormula(){
+        Random rand = new Random();
+        amount = rand.nextInt(3) + 1;
+
+        values = new int[5];
+        values[0] = rand.nextInt(9) + 1;
+        formula = "";
+
+        for (int i = 0; i < amount; i++){
+            switch (rand.nextInt(3)){
+                case 0: formula += " + ";
+                    break;
+                case 1: formula += " * ";
+                    break;
+                case 2: formula += " - ";
+                    break;
+            }
+            formula += Integer.toString(rand.nextInt(9) + 1);
+        }
+        System.out.println(formula);
+
+    }
+
+    public static double eval(final String str) {
+            return new Object() {
+                int pos = -1, ch;
+
+                void nextChar() {
+                    ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+                }
+
+                boolean eat(int charToEat) {
+                    while (ch == ' ') nextChar();
+                    if (ch == charToEat) {
+                        nextChar();
+                        return true;
+                    }
+                    return false;
+                }
+
+                double parse() {
+                    nextChar();
+                    double x = parseExpression();
+                    if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                    return x;
+                }
+
+                // Grammar:
+                // expression = term | expression `+` term | expression `-` term
+                // term = factor | term `*` factor | term `/` factor
+                // factor = `+` factor | `-` factor | `(` expression `)`
+                //        | number | functionName factor | factor `^` factor
+
+                double parseExpression() {
+                    double x = parseTerm();
+                    for (;;) {
+                        if      (eat('+')) x += parseTerm(); // addition
+                        else if (eat('-')) x -= parseTerm(); // subtraction
+                        else return x;
+                    }
+                }
+
+                double parseTerm() {
+                    double x = parseFactor();
+                    for (;;) {
+                        if      (eat('*')) x *= parseFactor(); // multiplication
+                        else if (eat('/')) x /= parseFactor(); // division
+                        else return x;
+                    }
+                }
+
+                double parseFactor() {
+                    if (eat('+')) return parseFactor(); // unary plus
+                    if (eat('-')) return -parseFactor(); // unary minus
+
+                    double x;
+                    int startPos = this.pos;
+                    if (eat('(')) { // parentheses
+                        x = parseExpression();
+                        eat(')');
+                    } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                        while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                        x = Double.parseDouble(str.substring(startPos, this.pos));
+                    } else if (ch >= 'a' && ch <= 'z') { // functions
+                        while (ch >= 'a' && ch <= 'z') nextChar();
+                        String func = str.substring(startPos, this.pos);
+                        x = parseFactor();
+                        if (func.equals("sqrt")) x = Math.sqrt(x);
+                        else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                        else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                        else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                        else throw new RuntimeException("Unknown function: " + func);
+                    } else {
+                        throw new RuntimeException("Unexpected: " + (char)ch);
+                    }
+
+                    if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                    return x;
+                }
+            }.parse();
+        }
+
+}
